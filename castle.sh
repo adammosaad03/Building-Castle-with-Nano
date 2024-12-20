5f408158548979ba62828bc76ad6cb835db087c8
adammosaad03 <mosaadadam647@gmail.com>
2024-12-20 15:59:25 -0500

    Create castle.sh

---
+++ castle.sh
@@ -0,0 +1,79 @@
+    1  echo hello terminal
+    2  echo hello nano
+    3  touch castle.sh
+    4  ls
+    5  nano castle.sh
+    6  bash castle.sh
+    7  nano castle.sh
+    8  bash castle.sh
+    9  nano castle.sh
+   10  bash castle.sh
+   11  nano castle.sh
+   12  bash castle.sh
+   13  nano catsle.sh
+   14  bash castle.sh
+   15  nano castle.sh
+   16  bash castle.sh
+   17  nano castle.sh
+   18  bash castle.sh
+   19  nano castle.sh
+   20  bash castle.sh
+   21  nano castle.sh
+   22  bash castle.sh
+   23  nano castle.sh
+   24  bash castle.sh
+   25  history > castle.sh
+    1  echo hello terminal
+    2  echo hello nano
+    3  touch castle.sh
+    4  ls
+    5  nano castle.sh
+    6  bash castle.sh
+    7  nano castle.sh
+    8  bash castle.sh
+    9  nano castle.sh
+   10  bash castle.sh
+   11  nano castle.sh
+   12  bash castle.sh
+   13  nano catsle.sh
+   14  bash castle.sh
+   15  nano castle.sh
+   16  bash castle.sh
+   17  nano castle.sh
+   18  bash castle.sh
+   19  nano castle.sh
+   20  bash castle.sh
+   21  nano castle.sh
+   22  bash castle.sh
+   23  nano castle.sh
+   24  bash castle.sh
+   25  history > castle.sh
+   26  history >> castle.sh
+    1  echo hello terminal
+    2  echo hello nano
+    3  touch castle.sh
+    4  ls
+    5  nano castle.sh
+    6  bash castle.sh
+    7  nano castle.sh
+    8  bash castle.sh
+    9  nano castle.sh
+   10  bash castle.sh
+   11  nano castle.sh
+   12  bash castle.sh
+   13  nano catsle.sh
+   14  bash castle.sh
+   15  nano castle.sh
+   16  bash castle.sh
+   17  nano castle.sh
+   18  bash castle.sh
+   19  nano castle.sh
+   20  bash castle.sh
+   21  nano castle.sh
+   22  bash castle.sh
+   23  nano castle.sh
+   24  bash castle.sh
+   25  history > castle.sh
+   26  history >> castle.sh
+   27  nano ~/.nano_history
+   28  history >> castle.sh
